dedb8d22c1d39378902ea007b99976e9d387acd5
Jake <Jake@Jakes-MacBook-Pro.local>
2013-03-30 13:04:47 +1300

    Project creation

---
+++ src/lz/lz.java
@@ -0,0 +1,17 @@
+package lz;
+
+/**
+ * @author Jake Bellamy 1130587 jrb46
+ * @author Michael Coleman 1144239 mjc62
+ */
+public class lz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
